93c54f5da0f43964244bf698e82831152cf15ccb
Rahul Kumar <128629336+R0ahulkd@users.noreply.github.com>
2026-04-02 14:49:38 +0530

    Var,let,const in JavaScript

--- 03_JavaScript/01_Console_log/01_intro.js
+++ 03_JavaScript/01_Console_log/01_intro.js
@@ -1,1 +1,14 @@
 console.log("Hello, World!");
+
+var a = 7;
+console.log("The value of a is",a)
+var a = 8;
+console.log("The value of a is",a)
+let b = 5;
+console.log("The value of a is",b)
+// let b = 6; // Gives error Re-declaration not possible in let
+b = 6
+console.log("The value of a is",b)
+
+const c = 10 // Re-declaration and Re-initialization not possible
+console.log("The value of a is",c)
