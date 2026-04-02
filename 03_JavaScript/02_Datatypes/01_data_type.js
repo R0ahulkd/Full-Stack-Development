eb7bf869c8565a7da52553fa39504095d5d7db12
Rahul Kumar <128629336+R0ahulkd@users.noreply.github.com>
2026-04-02 15:07:45 +0530

    Data Types in JavaScript

---
+++ 03_JavaScript/02_Datatypes/01_data_type.js
@@ -0,0 +1,16 @@
+let a = 5;
+console.log("The value of let a is",typeof(a),a)
+let b = null;
+console.log("The value of let a is",typeof(b),b)
+let c = true;
+console.log("The value of let a is",typeof(c),c)
+let d = BigInt(4587921141232);
+let e = BigInt(23n);
+console.log("The value of let a is",typeof(d),d)
+console.log("The value of let a is",typeof(e),e)
+let f = "Rahul Kumar"
+console.log("The value of let a is",typeof(f),f)
+let g = Symbol("%")
+console.log("The value of let a is",typeof(g),g)
+let h;
+console.log("The value of let a is",typeof(h),h)
