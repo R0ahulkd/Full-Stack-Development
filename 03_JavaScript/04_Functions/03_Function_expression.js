2f693c1e457a58878f20510ff21edd85077d2e41
Rahul Kumar <128629336+R0ahulkd@users.noreply.github.com>
2026-04-03 16:14:51 +0530

    Arrow Function

---
+++ 03_JavaScript/04_Functions/03_Function_expression.js
@@ -0,0 +1,6 @@
+const sub = function(a,b){
+    return a-b;
+}
+
+let subtract = sub(9,4)
+console.log(subtract);
---
+++ 03_JavaScript/04_Functions/04_arrow_function.js
@@ -0,0 +1,3 @@
+let div=(a,b)=>a/b;
+let division = div(9,3);
+console.log(division);
