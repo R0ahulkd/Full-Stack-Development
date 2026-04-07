e2f5f6e9e4dd8f8a55d108b5ec986edbfc3f6d92
Rahul Kumar <128629336+R0ahulkd@users.noreply.github.com>
2026-04-07 16:56:27 +0530

    math functions

---
+++ 03_JavaScript/07_Builtin_Functions/01_mathfunc.js
@@ -0,0 +1,25 @@
+let value = Math.max(8,7,20,5,15,47)
+console.log(value)
+
+let value1 = Math.min(8,7,20,5,15,47)
+console.log(value1)
+
+let value2 = Math.random()
+console.log(value2)
+
+let arr = [8,7,20,5,15,47];
+console.log(Math.min(...arr))
+
+let num = 12.56985;
+let val1 = Math.round(num); // Round off
+let val2 = Math.ceil(num); // Nearest Maximum
+let val3 = Math.floor(num); // Nearest lowest
+let val4 = Math.trunc(num) // Removes decimal
+console.log(val1);
+console.log(val2);
+console.log(val3);
+console.log(val4);
+
+console.log(Math.PI);
+console.log(Math.abs(-4));
+console.log(Math.pow(5,2));
---
+++ 03_JavaScript/07_Builtin_Functions/02_Stringmethods.js
@@ -0,0 +1,6 @@
+let user = "Hariom";
+// accessing character
+console.log(user.charAt(0));
+
+let subname = user.slice(0,4);
+console.log(subname);
