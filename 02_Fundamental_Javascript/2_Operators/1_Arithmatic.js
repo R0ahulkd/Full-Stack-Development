21d378a363050aa538e844b474dae7a8b4efd51e
r0ahulkd <r0ahulkd@gmail.com>
2026-09-15 17:47:38 +0530

    Arithmatic and Conditional Operators

---
+++ 02_Fundamental_Javascript/2_Operators/1_Arithmatic.js
@@ -0,0 +1,13 @@
+let a = 11;
+let b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+a += 1;
+console.log(a)
+
+console.log(typeof (a / b));
