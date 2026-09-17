57ae197215845657b6cd4f496cb69a803c287344
r0ahulkd <r0ahulkd@gmail.com>
2026-09-17 22:03:35 +0530

    Loops

---
+++ 02_Fundamental_Javascript/5_Loops/1_script.js
@@ -0,0 +1,25 @@
+for (let i = 1; i <= 5; i+=1) {
+    console.log(i);
+}
+
+// let is18 = false;
+// let age = 0;
+// while (!is18) {
+//     console.log("Child", age);
+//     age = age + 1;
+//     if (age == 18) {
+//         is18 = true;
+//     }
+// }
+
+// let counter = 0;
+// while (counter < 20) {
+//     counter = counter + 1;
+//     console.log(counter)
+// }
+
+let counter = 20;
+do {
+    console.log(counter);
+    counter++;
+} while (counter < 20)
